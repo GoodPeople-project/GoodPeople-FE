4c3c093096c3f6ae32d73738ac09fb9a384dee9f
chaduhwan <enghks2973@naver.com>
2025-04-09 15:33:46 +0900

    Chore: VanillaExtract config 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const baseConfig: NextConfig = {
   /* config options here */
 };
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
+const nextConfig = withVanillaExtract(baseConfig);
 
 export default nextConfig;
