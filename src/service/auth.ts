25fc96e88ab0a18219d12b5ab2bec6b7a2371f5b
chaduhwan <enghks2973@naver.com>
2025-05-11 15:00:56 +0900

    Fix: 회원가입 URL 수정

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -26,7 +26,7 @@ export async function Login(username: string, password: string) {
 
 export async function Signup(username: string, password: string, nickname: string) {
   try {
-    const response = await fetch(`${URL}/api/auth/signup`, {
+    const response = await fetch(`${URL}/api/user`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
